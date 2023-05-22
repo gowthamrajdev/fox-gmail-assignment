223ccbe787d8b3a02de76a250f62643107a1497a
gowthamrajdev <https://gowthamrajdev@github.com>
2023-05-22 20:44:39 +0530

    Added google auth success and failure routes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
+import passport from 'passport';
+import session from 'express-session';
+
 require('../initializers/passport-auth');
 
 const app = express();
@@ -13,6 +16,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(session({ secret: 'SECRET', resave: true, saveUninitialized: false })); 
+
+// passport initialization
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
 
 app.use('/', indexRouter);
--- src/routes/users.js
+++ src/routes/users.js
@@ -16,8 +16,18 @@ router.get('/auth', passport.authenticate('google', { scope:
 
 router.get('/auth/callback', 
       passport.authenticate( 'google', {
-          successRedirect: '/auth/google/success',
-          failureRedirect: '/auth/google/failure'
+          successRedirect: '/users/auth/google/success',
+          failureRedirect: '/users/auth/google/failure'
 }));
+
+router.get('/auth/google/success', (req, res, next) => {
+  console.log('===success====')
+  res.json({message: 'google login success'})
+});
+
+router.get('/auth/google/failure', (req, res, next) => {
+  console.log('===failure====')
+  res.json({message: 'google login failed'})
+});
 
 export default router;
